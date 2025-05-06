a8b5dbf8474368da4b31dc68b61608fa6933f5e3
bhanuprakash.t@perigorddata.com <bhanuprakash.t@perigorddata.com>
2025-05-06 19:13:24 +0530

    whole project

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+
+// not-found.js file is the default file name in next.js when ever we created this file 
+// next.js automatically find this file and renders when user wants to get the page which is not valid
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunatly we could not find the requested page or resource.</p>
+        </main>
+    )
+}
